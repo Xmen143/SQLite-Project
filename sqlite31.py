e6e8b9bf7105bd01c73f3570a457baefb3202aec
Xmen143 <83784820+Xmen143@users.noreply.github.com>
2021-09-03 17:09:16 +0530

    Add files via upload

--- sqlite31.py
+++ sqlite31.py
@@ -1,15 +1,20 @@
 import sqlite3
 
-
+#connect the database
 conn = sqlite3.connect('Mulesoft SQL.db')
 
 #craete a cursor
 c = conn.cursor()
 
+#craete a Query
 c.execute("SELECT * FROM Movies")
+
+c.execute("SELECT Name FROM Movies Where Actor = 'Salman Khan'")
 
 print(c.fetchall())
 
+#commit the db
 conn.commit
 
+#Close the db
 conn.close
